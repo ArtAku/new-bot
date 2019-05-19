90f90519b3c1f9f04adc715affb2e9f1baa1b82b
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-19 22:35:38 +0700

    init 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
-const bot = new TelegramBot(TOKEN, {polling = true})
+const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'Здравствуй!')
