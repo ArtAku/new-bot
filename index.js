792454b06b4ad1499188fb0bb52aa5c348a1f495
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-19 22:49:43 +0700

    init 3

--- index.js
+++ index.js
@@ -2,19 +2,6 @@ const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.onText(/^\/place_order/, function (msg, match) {
-    var option = {
-        "parse_mode": "Markdown",
-        "reply_markup": {
-            "one_time_keyboard": true,
-            "keyboard": [[{
-                text: "My phone number",
-                request_contact: true
-            }], ["Cancel"]]
-        }
-    };
-    bot.sendMessage(msg.chat.id, "How can we contact you?", option).then(() => {
-        // handle user phone
-    })
-
-});
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'Здравствуй!')
+})
