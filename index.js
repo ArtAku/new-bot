e962c6c1bc2c496e1eff34151ccedc24206d2e2c
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 12:52:32 +0700

    wip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on("contact", msg => {
+bot.on('message', msg => {
     var yes = "Yes";
     if (msg.text.indexOf(yes) === 0) {
         bot.sendMessage(msg.chat.id, "I will show you everything!");
@@ -15,12 +15,13 @@ bot.on("contact", msg => {
 })
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Здравствуй! Интересна погода?', {
+    var option = {
         "reply_markup": {
             "keyboard": [[{ 
                 text: "Yes",
                 request_location: true
             }], ["No"]]
-        },
-    })
+        }
+    }
+    bot.sendMessage(msg.chat.id, 'Здравствуй! Интересна погода?', option)
 })
