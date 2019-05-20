e72e089e17165c43f046ceb0725f232b5cd09ffb
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 15:03:33 +0700

    wip

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
@@ -9,13 +8,14 @@ bot.on('location', (msg) => {
     reqest_string += msg.location.latitude + '&lon=' + msg.location.longitude + '&appid=92b1a5b3125eff26a674219cc3f78775';
     bot.sendMessage(msg.chat.id, reqest_string);
 
-    axios.get(reqest_string, {
-        params: {
-            var: result
-        }
-    }).then(response => {
-        bot.sendMessage(msg.chat.id, result);
-  })
+    const request = require('request');
+
+    request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+        bot.sendMessage(msg.chat.id, body.url);
+        if (err) { return console.log(err); }
+        console.log(body.url);
+        console.log(body.explanation);
+    });
 });
 
 bot.onText(/\/start/, (msg) => {
