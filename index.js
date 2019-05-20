54e8179394bd507bd5a9432e9ac007b4ebbc0d50
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 15:20:29 +0700

    wip

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on('location', (msg) => {
 
     request(reqest_string, { json: true }, (err, res, body) => {
         bot.sendMessage(msg.chat.id, "I'm in");
-        bot.sendMessage(msg.chat.id, body);
+        bot.sendMessage(msg.chat.id, body.explanation);
         if (err) { return console.log(err); }
         console.log(body.url);
         console.log(body.explanation);
