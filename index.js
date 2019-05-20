16b4d4424867c47859765de88d85a369b633e224
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 14:41:28 +0700

    wip

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const https = require("https")
+const axios = require('axios');
 const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('location', (msg) => {
-    bot.sendMessage(msg.chat.id, "I will show you everything!" + msg.location.latitude + " " + msg.location.longitude)
-    var reqest_string = 'https://api.openweathermap.org/data/2.5/weather?lat='
-    reqest_string += msg.location.latitude + '&lon=' + msg.location.longitude + '&appid=92b1a5b3125eff26a674219cc3f78775'
-    bot.sendMessage(msg.chat.id, reqest_string)
+    bot.sendMessage(msg.chat.id, "I will show you everything!" + msg.location.latitude + " " + msg.location.longitude);
+    var reqest_string = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+    reqest_string += msg.location.latitude + '&lon=' + msg.location.longitude + '&appid=92b1a5b3125eff26a674219cc3f78775';
+    bot.sendMessage(msg.chat.id, reqest_string);
 
-    https.get(reqest_string, (res) =>{
-        bot.sendMessage(msg.chat.id, "Work");
-        bot.sendMessage(msg.chat.id, res);
-    })
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(response => {
+        bot.sendMessage(msg.chat.id, "alive");
+  })
 });
 
 bot.onText(/\/start/, (msg) => {
