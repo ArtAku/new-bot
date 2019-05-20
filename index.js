7223c7f607cd69cd21d73f3b183ecee5ad0788e8
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 12:29:28 +0700

    wip

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ bot.on('message', msg => {
     } 
 })
 
+bot.onReplyToMessage('message', msg => {
+    var yes = "Yes";
+    if (msg.text.indexOf(yes) === 0) {
+        bot.sendMessage(msg.chat.id, "I will show you everything!", [msg.location.longitude,msg.location.latitude].join(";"));
+    }
+
+    var no = "No";
+    if (msg.text.indexOf(no) === 0) {
+        bot.sendMessage(msg.chat.id, "Then just leave!");
+    } 
+})
+
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Здравствуй! Интересна погода?', {
         "reply_markup": {
