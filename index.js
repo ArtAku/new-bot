e3c1db5595feb96dd8a5a8a09953c3bf35e374d4
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 12:44:32 +0700

    wip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
+bot.on("contact", msg => {
     var yes = "Yes";
     if (msg.text.indexOf(yes) === 0) {
         bot.sendMessage(msg.chat.id, "I will show you everything!");
