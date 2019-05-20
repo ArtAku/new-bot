8afc6c549719f356d148930290680677f07c36ba
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 16:06:12 +0700

    wip

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ bot.on('location', (msg) => {
     var request_string = 'https://api.openweathermap.org/data/2.5/weather?lat=';
     reqest_string += msg.location.latitude + '&lon=' + msg.location.longitude + '&mode=html' + '&appid=92b1a5b3125eff26a674219cc3f78775';
     bot.sendMessage(msg.chat.id, " Здесь ты можешь посмотреть погоду: " + request_string);
-
-    const request = require('request');
-
-    request(reqest_string, { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        bot.sendMessage(msg.chat.id, "I'm in");
-    });
 });
 
 bot.onText(/\/start/, (msg) => {
