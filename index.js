0bd57085506db2b3e549e1f7d31d09d301cdcba7
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 11:56:03 +0700

    weather 5

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ bot.on('message', msg => {
     var yes = "Yes";
     if (msg.text.indexOf(yes) === 0) {
         bot.sendMessage(msg.chat.id, "I will show you everything!");
+        bot.sendMessage(msg.chat.id, msg.location.longitude + " " + msg.location.latitude);
     }
 
     var no = "No";
