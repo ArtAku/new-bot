3f28901915780d20a09757eb342864c1e4110051
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 11:39:34 +0700

    weather 4

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-
 bot.on('message', msg => {
     var yes = "Yes";
     if (msg.text.indexOf(yes) === 0) {
@@ -18,7 +17,10 @@ bot.on('message', msg => {
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Здравствуй! Интересна погода?', {
         "reply_markup": {
-            "keyboard": [["Yes", request_location], ["No"]]
-        }
+            "keyboard": [[{ 
+                text: "Yes",
+                request_location: true
+            }], ["No"]]
+        },
     })
 })
