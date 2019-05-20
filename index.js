160cc18eeec1dcc81c0c875e262b0e715176a765
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 13:38:34 +0700

    wip

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
-
-bot.on('message', msg => {
-    var yes = "Yes";
-    if (msg.text.length == 0) {
-        bot.sendMessage(msg.chat.id, "I will show you everything!");
-    }
-
-    var no = "No";
-    if (msg.text.indexOf(no) === 0) {
-        bot.sendMessage(msg.chat.id, "Then just leave!");
-    } 
-});
-
-bot.onText(/getLocation/, (msg) => {
-    const opts = {
-      reply_markup: JSON.stringify({
-        keyboard: [
-          [{text: 'Location', request_location: true}],
-          [{text: 'Contact', request_contact: true}],
-        ],
-        resize_keyboard: true,
-        one_time_keyboard: true,
-      }),
-    };
-    bot.sendMessage(msg.chat.id, 'Contact and Location request', opts);
-});
+var loc = false;
 
 bot.on('location', (msg) => {
     bot.sendMessage(msg.chat.id, "I will show you everything!");
