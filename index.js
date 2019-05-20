ab801109046efb8fe477c79c439608fe927c02e0
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-20 12:22:32 +0700

    weather 6

--- index.js
+++ index.js
@@ -2,17 +2,7 @@ const TOKEN = '693092471:AAEO3JdTxgcvClWK6Cp-s5GqaEdSs94TLzE'
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
-    var yes = "Yes";
-    if (msg.text.indexOf(yes) === 0) {
-        bot.sendMessage(msg.chat.id, "I will show you everything!", [msg.location.longitude,msg.location.latitude].join(";"));
-    }
 
-    var no = "No";
-    if (msg.text.indexOf(no) === 0) {
-        bot.sendMessage(msg.chat.id, "Then just leave!");
-    } 
-})
 
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Здравствуй! Интересна погода?', {
@@ -22,5 +12,18 @@ bot.onText(/\/start/, (msg) => {
                 request_location: true
             }], ["No"]]
         },
-    })
+    }).then(() => {
+        bot.on("message", msg => {
+            var yes = "Yes";
+            if (msg.text.indexOf(yes) === 0) {
+               bot.sendMessage(msg.chat.id, "I will show you everything!", [msg.location.longitude,msg.location.latitude].join(";"));
+             }
+
+            var no = "No";
+            if (msg.text.indexOf(no) === 0) {
+                bot.sendMessage(msg.chat.id, "Then just leave!");
+            } 
+        })
+    })   
 })
+
